f02c6ccd932fdc3e3b9b2142227cf0cec6657af2
Udit <udit.sharma2021@vitbhopal.ac.in>
2022-10-30 23:59:52 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:flash_chat/screens/welcome_screen.dart';
+import 'package:flash_chat/screens/login_screen.dart';
+import 'package:flash_chat/screens/registration_screen.dart';
+import 'package:flash_chat/screens/chat_screen.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(FlashChat());
+}
+
+class FlashChat extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.dark(),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => WelcomeScreen(),
+        '/l': (context) => LoginScreen(),
+        '/r': (context) => RegistrationScreen(),
+        '/c': (context) => ChatScreen()
+      },
+    );
+  }
+}
